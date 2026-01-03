06060fc9b4371db79424ceb2ba2e90714950fdef
Jeongwoo Park <wahoo9040@naver.com>
2026-01-03 10:46:29 +0900

    feat: integrate TokenService into UserService for enhanced token management

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,9 @@
 pub mod assist_service;
 pub mod memo_service;
+pub mod token_service;
 pub mod user_service;
 
 pub use assist_service::AssistService;
 pub use memo_service::MemoService;
+pub use token_service::TokenService;
 pub use user_service::UserService;
